155e6bd29eedfa79ff7086cc0890a597856e7d40
Thomas Rothe <nomad.blow@gmail.com>
2017-10-24 17:06:08 +0800

    [B] fixed typo, add history validation for length [0,100], add IN validator for riskLevel, complianceStatus, status

--- src/common/models/asset.js
+++ src/common/models/asset.js
@@ -2,12 +2,15 @@
 
 module.exports = function(Asset) {
 
+    Asset.validatesPresenceOf(  'model', 'serial', 'batchNo', 
+                                'description', 'createDate', 'productionDate' );
+
     Asset.validatesLengthOf('model', {
         min: 3,
         max: 100,
         message: {
             min: 'The asset model name should be at least 3 characters long',
-            max: 'The asset model name it to long'
+            max: 'The asset model name is to long'
         }
     })
 
@@ -16,7 +19,7 @@ module.exports = function(Asset) {
         max: 100,
         message: {
             min: 'The asset serial number should be at least 3 characters long',
-            max: 'The asset serial number it to long'
+            max: 'The asset serial number is to long'
         }
     })
 
@@ -25,7 +28,7 @@ module.exports = function(Asset) {
         max: 100,
         message: {
             min: 'The asset batch number should be at least 3 characters long',
-            max: 'The asset batch number it to long'
+            max: 'The asset batch number is to long'
         }
     })
 
@@ -34,10 +37,28 @@ module.exports = function(Asset) {
         max: 100,
         message: {
             min: 'The asset description should be at least 5 characters long',
-            max: 'The asset description it to long'
+            max: 'The asset description is to long'
         }
     })
 
+    Asset.validatesLengthOf('history', {
+        max: 100,
+        message: {
+            max: 'The asset history is to long'
+        }
+    })
+
+    Asset.validatesInclusionOf('riskLevel', {
+        in: ['OK'], message: 'is not allowed' 
+    });
+
+    Asset.validatesInclusionOf('complianceStatus', {
+        in: ['COMPLIANT'], message: 'is not allowed' 
+    });
+
+    Asset.validatesInclusionOf('status', {
+        in: ['OK', 'OVERDUE INSPECTITON', 'DO NOT USE'], message: 'is not allowed' 
+    });
 
 
 
