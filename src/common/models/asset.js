32ad9e706a32d660f7fc69dfb34c28c31f2a9ddd
Thomas Rothe <nomad.blow@gmail.com>
2017-10-24 15:14:14 +0800

    [B] add validator for min, max for property description

--- src/common/models/asset.js
+++ src/common/models/asset.js
@@ -2,4 +2,13 @@
 
 module.exports = function(Asset) {
 
+    Asset.validatesLengthOf('description', {
+        min: 5,
+        max: 100,
+        message: {
+            min: 'The asset description should be at least 5 characters long',
+            max: 'The asset description it to long'
+        }
+    })
+
 };
