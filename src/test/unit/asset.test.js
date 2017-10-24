cd313dae652d7282182b058546eab87c892971fb
Thomas Rothe <nomad.blow@gmail.com>
2017-10-24 14:39:08 +0800

    [B] create route test for Asset API route

--- src/test/unit/asset.test.js
+++ src/test/unit/asset.test.js
@@ -1,4 +1,4 @@
-const { app, expect } = require('../common')
+const { app, expect, request } = require('../common')
 
 const Asset = app.models.Asset
 
@@ -7,5 +7,23 @@ describe('It should resolve', function() {
         return Asset
             .find()
             .then(res => console.log(res))
+    })
+})
+
+describe('It should resolve the routes', function() {
+
+    var Asset
+
+    before(function() {
+        Asset = app.models.Asset
+    })
+
+    beforeEach(function(done) {
+        Asset.create({ "model": "", "serial": "", "batch-no": "", "description": "" }, 
+            function() { done() })
+    })
+
+    it('to post a new asset', function(done) {
+        request.post('/api/Assets').send({  }).expect(200, done)
     })
 })
