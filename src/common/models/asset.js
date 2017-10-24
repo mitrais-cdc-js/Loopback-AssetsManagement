d25afdb1c040346a171dbdb4446528b09dad5d4b
Thomas Rothe <nomad.blow@gmail.com>
2017-10-24 15:21:49 +0800

    [B] add validation for length for model, serial, batchNo

--- src/common/models/asset.js
+++ src/common/models/asset.js
@@ -2,6 +2,33 @@
 
 module.exports = function(Asset) {
 
+    Asset.validatesLengthOf('model', {
+        min: 3,
+        max: 100,
+        message: {
+            min: 'The asset model name should be at least 3 characters long',
+            max: 'The asset model name it to long'
+        }
+    })
+
+    Asset.validatesLengthOf('serial', {
+        min: 3,
+        max: 100,
+        message: {
+            min: 'The asset serial number should be at least 3 characters long',
+            max: 'The asset serial number it to long'
+        }
+    })
+
+    Asset.validatesLengthOf('batchNo', {
+        min: 3,
+        max: 100,
+        message: {
+            min: 'The asset batch number should be at least 3 characters long',
+            max: 'The asset batch number it to long'
+        }
+    })
+
     Asset.validatesLengthOf('description', {
         min: 5,
         max: 100,
@@ -10,5 +37,8 @@ module.exports = function(Asset) {
             max: 'The asset description it to long'
         }
     })
+
+
+
 
 };
