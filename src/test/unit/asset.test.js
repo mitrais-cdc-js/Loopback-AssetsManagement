84c6632149ed15dd3648c45a79dfd99a56e91291
Thomas Rothe <nomad.blow@gmail.com>
2017-10-24 15:48:46 +0800

    [B] add unit test for creating entity asset with mandatory fields

--- src/test/unit/asset.test.js
+++ src/test/unit/asset.test.js
@@ -23,7 +23,21 @@ describe('It should resolve the routes', function() {
             function() { done() })
     })
 
-    it('to post a new asset', function(done) {
-        request.post('/api/Assets').send({  }).expect(200, done)
+    it('to post a proper filled asset and succeed', function(done) {
+        request.post('/api/Assets').send({ 
+                "model": "HP Probook", 
+                "serial": "2133-3414-111",
+                "batchNo": "334912",
+                "description": "work-station",
+                "createDate": "2017-04-23"}).expect(200, done)
+    })
+
+    it('to post a blank-model-name asset and fail', function(done) {
+        request.post('/api/Assets').send({ 
+                "model": "", 
+                "serial": "2133-3414-111",
+                "batchNo": "334912",
+                "description": "work-station",
+                "createDate": "2017-04-23"}).expect(422, done)
     })
 })
