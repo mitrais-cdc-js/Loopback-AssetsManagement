abd31dbbaad327f06912cedfa114ebd9084419e7
throthe <nomad.blow@gmail.com>
2017-11-06 11:02:56 +0800

    [B] fix small bug when route is not properly set, set route as const, increase logging

--- src/server/boot/routes.js
+++ src/server/boot/routes.js
@@ -1,16 +1,25 @@
 module.exports = function(app) {
     // Install a "/ping" route that returns "pong"
 
-    app.get('/api/assets-p', function(req, res){
-        console.log(req.query._sort);
-        console.log(req.query._order);
-        console.log(req.query._page);
-        console.log(req.query._limit);
+    const CUSTOM_IN_ROUTE_ASSET_PAGING = '/api/assets-p';
+    const CUSTOM_OUT_ROUTE_ASSET_PAGING = '/api/assets';
 
-        var skip = (req.query._page - 1) * req.query._limit;
+    app.get(`${CUSTOM_IN_ROUTE_ASSET_PAGING}`, function(req, res){
 
-        console.log(`/api/assets?filter[order]=${req.query._sort} ${req.query._order}&filter[limit]=${req.query._limit}&filter[skip]=${skip}`);
-        res.redirect(`/api/assets?filter[order]=${req.query._sort} ${req.query._order}&filter[limit]=${req.query._limit}&filter[skip]=${skip}`);
-       //res.redirect(`/api/assets?filter[limit]=${req.query._limit}&filter[skip]=${skip}`);
+        console.log(`incoming custom route: ${CUSTOM_IN_ROUTE_ASSET_PAGING}?_sort=${req.query._sort}&_order=${req.query._order}&_limit=${req.query._limit}&_page=${req.query._page}`);
+
+        if(!req.query._sort || !req.query._order) {
+            res.redirect(`${CUSTOM_OUT_ROUTE_ASSET_PAGING}`);
+        }
+
+        var skip = (req.query._page - 1) * (req.query._limit - 1);
+
+        if(isNaN(skip)) {
+            skip = 0;
+        }
+
+        console.log(`outgoing custom route: ${CUSTOM_OUT_ROUTE_ASSET_PAGING}?filter[order]=${req.query._sort} ${req.query._order}&filter[limit]=${req.query._limit}&filter[skip]=${skip}`);   
+       
+        res.redirect(`${CUSTOM_OUT_ROUTE_ASSET_PAGING}?filter[order]=${req.query._sort} ${req.query._order}&filter[limit]=${req.query._limit}&filter[skip]=${skip}`);
     });
   }
