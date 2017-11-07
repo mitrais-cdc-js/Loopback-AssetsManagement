5220e2d6291479a2deeba80e3b5cedb44509b339
William Leonardo <williaml@mitrais.com>
2017-11-07 10:49:56 +0700

    remove .only from asset.test, was using that to isolate the test

--- src/test/unit/asset.test.js
+++ src/test/unit/asset.test.js
@@ -103,7 +103,7 @@ describe('It should resolve the routes', function() {
             .expect(404)
     })
     
-    it.only('to get all assets ordered by latest created date first and succeed', function() {
+    it('to get all assets ordered by latest created date first and succeed', function() {
         return request
             .get('/api/assets?filter=%7B%22order%22%3A%20%22createDate%20desc%22%20%7D')
             .expect(200)
