e8ba80afb3780f8cf80920ead80cd5032635ea41
throthe <nomad.blow@gmail.com>
2017-11-10 14:33:22 +0800

    [UI] transform date to ISO string

--- ui/src/app/services/data.services.ts
+++ ui/src/app/services/data.services.ts
@@ -85,6 +85,8 @@ export class DataService {
         if (asset.productionDate) {
             if( this.parseDateFormat(asset.productionDate, "DD/MM/YYYY") ) {
                 asset.productionDate = new Date(this.convertStringToDate(asset.productionDate));
+                asset.productionDate = asset.productionDate.toISOString();
+                console.log(asset.productionDate);
             } else {
                 throw new TypeError("Production Date is not valid formated in DD/MM/YYYY");
             }
@@ -93,6 +95,7 @@ export class DataService {
         if (asset.installedDate) {
             if( this.parseDateFormat(asset.installedDate, "DD/MM/YYYY") ) {
                 asset.installedDate = new Date(this.convertStringToDate(asset.installedDate));
+                asset.installedDate = asset.installedDate.toISOString();
             } else {
                 throw new TypeError("Installation Date is not valid formated in DD/MM/YYYY");
             }
@@ -101,6 +104,7 @@ export class DataService {
         if (asset.scheduledReplacementDate) {
             if( this.parseDateFormat(asset.scheduledReplacementDate, "DD/MM/YYYY") ) {
                 asset.scheduledReplacementDate = new Date(this.convertStringToDate(asset.scheduledReplacementDate));
+                asset.scheduledReplacementDate = asset.scheduledReplacementDate.toISOString();
             } else {
                 throw new TypeError("Schedule Replacement Date is not valid formated in DD/MM/YYYY");
             }
@@ -109,6 +113,7 @@ export class DataService {
         if (asset.lastRecertificationDate) {
             if( this.parseDateFormat(asset.lastRecertificationDate, "DD/MM/YYYY") ) {
                 asset.lastRecertificationDate = new Date(this.convertStringToDate(asset.lastRecertificationDate));
+                asset.lastRecertificationDate = asset.lastRecertificationDate.toISOString();
             } else {
                 throw new TypeError("Last Recertification Date is not valid formated in DD/MM/YYYY");
             }
@@ -117,6 +122,7 @@ export class DataService {
         if (asset.nextRecertificationDate) {
             if( this.parseDateFormat(asset.nextRecertificationDate, "DD/MM/YYYY") ) {
                 asset.nextRecertificationDate = new Date(this.convertStringToDate(asset.nextRecertificationDate));
+                asset.nextRecertificationDate = asset.nextRecertificationDate.toISOString();
             } else {
                 throw new TypeError("Next Recertification Date is not valid formated in DD/MM/YYYY");
             }
