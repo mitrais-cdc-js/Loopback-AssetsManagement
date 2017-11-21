69f8468a01ecce8bf84578dcf80f39b476ffd759
William Leonardo <williaml@mitrais.com>
2017-11-21 14:28:03 +0700

    Implement auto calculation for Scheduled Replacement Date

--- ui/src/app/asset/asset-create/asset-create.component.ts
+++ ui/src/app/asset/asset-create/asset-create.component.ts
@@ -4,33 +4,33 @@ import { NgForm } from '@angular/forms';
 
 // services
 import { DataService } from '../../services/data.services';
+import { UtilityService } from '../../services/utility.services';
 
 // model
 import { Asset } from './../asset';
-
 
 @Component({
   selector: 'app-asset-create',
   templateUrl: './asset-create.component.html',
   styleUrls: ['./asset-create.component.css']
 })
+
 export class AssetCreateComponent implements OnInit {
   title = 'Asset Management Application';
   autoCalculate: boolean = true
+  dateOfInstallation: string
+  dateOfReplacement: string
+  warranty: number
 
-  // newAsset = new Asset();
-
-  constructor( private dataService: DataService, private router: Router ) {
+  constructor( private dataService: DataService, private utilityService: UtilityService, private router: Router ) {
     console.log('AssetCreateComponent const called...');
   }
 
   ngOnInit() {
     console.log('ngOnInit called...');
-    // this.dataService
   }
 
   createAsset(form: NgForm) {
-
     const newAsset = form.value;
     newAsset.geolocation = (newAsset.geolocation === '') ? null : newAsset.geolocation;
     newAsset.installedDate = (newAsset.installedDate === '') ? null : newAsset.installedDate;
@@ -51,6 +51,17 @@ export class AssetCreateComponent implements OnInit {
 
   toggleAutoCalculate() {
     this.autoCalculate = !this.autoCalculate
+    this.recalculateReplacementDate()
+  }
+
+  recalculateReplacementDate() {
+    if(this.autoCalculate && this.dateOfInstallation !== null && this.warranty !== null) {
+      const days = this.warranty * 7
+      var tempDate = new Date(this.dataService.convertStringToDate(this.dateOfInstallation))
+
+      tempDate = new Date(tempDate.getTime() + (days * 86400000))
+      this.dateOfReplacement = this.utilityService.dateFormat(tempDate)
+    }
   }
 }
 
