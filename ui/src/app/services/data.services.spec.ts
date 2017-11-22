0cbcc7912d0081d9eb9e435bd10beeb490f003ab
throthe <nomad.blow@gmail.com>
2017-11-22 09:46:59 +0800

    add test for throwing exeption is NULL

--- ui/src/app/services/data.services.spec.ts
+++ ui/src/app/services/data.services.spec.ts
@@ -3,6 +3,7 @@ import { HttpModule } from '@angular/http';
 
 import { DataService } from './data.services';
 import { ServiceError, ServiceResultCode } from './serviceResult';
+import { Asset } from './../asset/asset';
 
 describe('Data Service', () => {
   beforeEach(() => {
@@ -43,5 +44,13 @@ describe('Data Service', () => {
     expect(function() { service.patchAsset(null); }).toThrowError(ServiceError);
     expect(function() { service.patchAsset(undefined); }).toThrowError(ServiceError);
   }));
+
+  it('should throw ...', inject([DataService], (service: DataService) => {
+    const ass: Asset = new Asset('test', '12345677', '12345678', '11/11/1111', '', '');
+
+    expect(function() { service.patchAsset(null); }).toThrowError(ServiceError);
+    expect(function() { service.patchAsset(undefined); }).toThrowError(ServiceError);
+  }));
+
 
 });
