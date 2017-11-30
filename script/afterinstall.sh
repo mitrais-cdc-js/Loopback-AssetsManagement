84eb647b210debecf1d5f510fb7f471fbddacc58
William Leonardo <williaml@mitrais.com>
2017-11-30 15:32:26 +0700

    backup node_modules on staging server to reduce npm install burden for each deployment

--- script/afterinstall.sh
+++ script/afterinstall.sh
@@ -4,6 +4,7 @@ cd /home/ubuntu/server/
 sudo chmod 777 -R /home/ubuntu/server/backend
 sudo chmod 777 -R /home/ubuntu/server/frontend
 cd /home/ubuntu/server/backend
+sudo mv /home/ubuntu/server/node_modules /home/ubuntu/server/backend/node_modules
 npm install
 cd /home/ubuntu/server/backend/server
 pm2 restart loopback --update-env
--- script/beforeinstall.sh
+++ script/beforeinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+sudo mv /home/ubuntu/server/backend/node_modules /home/ubuntu/server/node_modules
 sudo rm -rf /home/ubuntu/server/backend
 sudo rm -rf /home/ubuntu/server/frontend
