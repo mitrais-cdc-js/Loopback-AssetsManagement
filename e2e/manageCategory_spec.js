fde2e30422e17e4964af639454e97f1c75530ae6
gdhadi <igede.nurhadi@gmail.com>
2017-12-13 14:12:49 +0800

    Add scenario for create and view

--- e2e/manageCategory_spec.js
+++ e2e/manageCategory_spec.js
@@ -5,6 +5,10 @@ var actInputDesc = new categoryPage ();
 var actClickSaveBtn = new categoryPage ();
 var exaccessCatList =  new categoryPage();
 var actDelAssetCat = new categoryPage ();
+var actClickInputParent = new categoryPage();
+var actClickInputChecklist = new categoryPage();
+var actInputParent = new categoryPage();
+var actInputChecklist = new categoryPage();
 
 beforeEach(function(){
     var accessHomepage = new categoryPage();
@@ -13,7 +17,6 @@ beforeEach(function(){
 
 });
 
-//describe ("manage asset category", function(){
 describe ("2916 create asset classification", function(){    
     it("000 access view category", function(){
         var actclickCatPage = new categoryPage ();
@@ -46,7 +49,7 @@ describe ("2916 create asset classification", function(){
 
     });
 
-    it("003 create asset category and display success message", function(){  
+    it("003 create asset category with mandatory field and display success message", function(){  
        var actclickCatPage = new categoryPage ();
        actclickCatPage.clickcatlink();
        actnewCatBtn.clickNewCat();
@@ -57,7 +60,6 @@ describe ("2916 create asset classification", function(){
        actInputDesc.inputDesc ();
        actClickSaveBtn.clickSaveBtn ();
        //expect success message displayed
-
        expect(element(by.css('.alert-success')).isPresent()).toBe(true);
 
        browser.driver.sleep(1000);
@@ -67,6 +69,7 @@ describe ("2916 create asset classification", function(){
        exaccessCatList.accessViewCat();
 
     });
+    
     it("003, 004 create duplicate asset category and display fail message", function(){  
         var actclickCatPage = new categoryPage ();
         actclickCatPage.clickcatlink();
@@ -104,6 +107,27 @@ describe ("2916 create asset classification", function(){
         //expect back to view category page
         exaccessCatList.accessCreateCat();
      });
+     it("000,003 successfully edit category", function(){
+        var actclickCatPage = new categoryPage ();
+        actclickCatPage.clickcatlink();
+        var actclickEdit = new categoryPage();
+        actclickEdit.clickeditLink();
+        //expect in edit category page
+        var exgetEditURL = new categoryPage();
+        exgetEditURL.getEditURL();
+
+        //edit category details
+        var actEditInputName = new categoryPage ();
+        actEditInputName.editInputName();
+        var actEditInputDesc = new categoryPage();
+        actEditInputDesc.editInputDesc();
+        var actclickEditSaveBtn = new categoryPage();
+        actclickEditSaveBtn.clickEditSaveBtn(); 
+
+        //expect success message displayed
+        expect(element(by.css('.alert-success')).isPresent()).toBe(true);
+
+     });
      it ("000 delete asset category", function(){ 
         var actclickCatPage = new categoryPage ();
         actclickCatPage.clickcatlink();
@@ -117,6 +141,55 @@ describe ("2916 create asset classification", function(){
     
         browser.driver.sleep(1000);
         browser.waitForAngular();
-    
       });
+
+      it("003 create asset category with all available field and display success message", function(){  
+        var actclickCatPage = new categoryPage ();
+        actclickCatPage.clickcatlink();
+        actnewCatBtn.clickNewCat();
+        //expect correct create asset category page
+        actnewCatBtn.accessCreateCat();
+ 
+        actInputName.inputName ();
+        actInputDesc.inputDesc ();
+        actInputParent.inputParent();
+        actClickInputChecklist.inputChecklist();
+
+        actClickSaveBtn.clickSaveBtn ();
+        //expect success message displayed
+        expect(element(by.css('.alert-success')).isPresent()).toBe(true);
+ 
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+  
+        //expect back to view category page
+        exaccessCatList.accessViewCat();
+     });
+
+     it ("000 delete asset category", function(){ 
+        var actclickCatPage = new categoryPage ();
+        actclickCatPage.clickcatlink();
+        actDelAssetCat.clickDelCatBtn();
+        
+        var timeoutInMilliseconds = 1000;
+        browser.wait(protractor.ExpectedConditions.alertIsPresent(), timeoutInMilliseconds);
+    
+        var alert = browser.switchTo().alert();
+        alert.accept();
+    
+        browser.driver.sleep(1000);
+        browser.waitForAngular();
+      });
+
+     it("000 view category asset detail",function(){
+        var actclickCatPage = new categoryPage ();
+        actclickCatPage.clickcatlink();
+        var actclickViewLink = new categoryPage();
+        actclickCatPage.clickViewLink();
+        //expect dispay view asset detail
+        var exViewDetailCat = new categoryPage();
+        exViewDetailCat.getViewLink();
+
+     });
+      
 });
--- e2e/page/category.po.js
+++ e2e/page/category.po.js
@@ -3,7 +3,9 @@ var categoryPage = function (){
     var catLink = element (by.css('[href="/categories"]'));
     var newCatBtn = element (by.xpath('/html/body/div/div/app-root/app-category/p[1]/a'))
     var DelCatBtn = element (by.xpath('/html/body/div/div/app-root/app-category/p[3]/ng2-smart-table/table/tbody/tr[1]/td[1]/ng2-st-tbody-edit-delete/a'));
-    
+    var editLink = element (by.xpath('/html/body/div/div/app-root/app-category/p[3]/ng2-smart-table/table/tbody/tr[1]/td[1]/ng2-st-tbody-custom/a[2]'));
+    var viewLink = element (by.xpath('/html/body/div/div/app-root/app-category/p[3]/ng2-smart-table/table/tbody/tr[1]/td[1]/ng2-st-tbody-custom/a[1]'));
+
     //element create category
     var name = element (by.xpath('/html/body/div/div/app-root/app-category-create/div/div[1]/div/form/div[1]/input'));
     var nameLabel = element(by.xpath('/html/body/div/div/app-root/app-category-create/div/div[1]/div/form/div[1]/label'));
@@ -12,6 +14,14 @@ var categoryPage = function (){
     var saveBtn = element (by.xpath('/html/body/div/div/app-root/app-category-create/div/div[1]/div/form/button'));
     var parentLabel = element (by.xpath('/html/body/div/div/app-root/app-category-create/div/div[1]/div/form/div[3]/label'));
     var checkLabel = element (by.xpath('/html/body/div/div/app-root/app-category-create/div/div[1]/div/form/div[4]/label'));
+    var parent = element (by.xpath('/html/body/div/div/app-root/app-category-create/div/div[1]/div/form/div[3]/select'));
+    var checklist = element(by.xpath('/html/body/div/div/app-root/app-category-create/div/div[1]/div/form/div[4]/select'));
+
+    //element edit category
+    var editName = element(by.xpath('/html/body/div/div/app-root/app-category-edit/div/div[1]/div/form/div[1]/input'));
+    var editDesc = element(by.xpath('/html/body/div/div/app-root/app-category-edit/div/div[1]/div/form/div[2]/input'));
+    var editSaveBtn = element (by.xpath('/html/body/div/div/app-root/app-category-edit/div/div[1]/div/form/button'));
+
 
     //action: access apps url
     this.get = function(){
@@ -42,13 +52,41 @@ var categoryPage = function (){
     this.inputName = function(){
         name.sendKeys("AMonitor_Auto");
     };
+    //action: edit input category name
+    this.editInputName = function(){
+        editName.sendKeys("AMonitor_Auto_edit");
+    };
     //action: input category description
     this.inputDesc = function (){
         desc.sendKeys("AMonitor,LCD_Auto");
     };
+    //action: edit input category description
+    this.editInputDesc = function(){
+        editDesc.sendKeys("AMonitor_Auto_edit");
+    };
+    //action: click input parent
+    this.clickInputParent = function(){
+        inputParent.click();
+    };
+    //action: input parent
+    this.inputParent = function(){
+        parent.sendKeys("Computer");
+    };
+    //action: click input checklist
+    this.clickInputChecklist = function(){
+        inputChecklist.click();
+    };
+    //action: input checklist
+    this.inputChecklist = function (){
+        checklist.sendKeys("Personal Computer");
+    };
     //action: click save button
     this.clickSaveBtn = function(){
         saveBtn.click();
+    };
+    //action: click edit save button
+    this.clickEditSaveBtn = function(){
+        editSaveBtn.click();
     };
     //expect: back to list of category and new category displayed
     this.accessViewCat = function(){
@@ -65,16 +103,30 @@ var categoryPage = function (){
     //expcet: category desc is displayed
     this.getcatDesc = function (){
         expect(descLabel.getText()).toEqual("Description*");
-
     };
     //expect: parent category is displayed
     this.getcatParent = function (){
         expect(parentLabel.getText()).toEqual("Parent Category");
-
     };
     //expect: checklist category is displayed
     this.getcatChecklist = function (){
         expect(checkLabel.getText()).toEqual("Checklist");
+    };
+    //action: click edit link
+    this.clickeditLink = function (){
+        editLink.click();
+    };
+    //expect: edit asset category URL
+    this.getEditURL = function (){
+        expect(browser.getCurrentUrl()).toContain("http://13.228.106.128/categories/edit/5a"); 
+    };
+    //action: click view link
+    this.clickViewLink = function(){
+        viewLink.click();
+    };
+    //expect: view category detail
+    this.getViewLink =function(){
+        expect(browser.getCurrentUrl()).toContain("http://13.228.106.128/categories/5a");
     };
 
 };
